fb6723c5c72fe48caca52b82ecb2f081253139ab
David Armstrong <wafflesultimate1@gmail.com>
2023-10-22 23:30:58 -0700

    Completed Exercise 2

--- PalindromeExercise/PalindromeExercise/Program.cs
+++ PalindromeExercise/PalindromeExercise/Program.cs
@@ -1,12 +1,28 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace PalindromeExercise
 {
+
+    public class WordSmith
+    {
+          public bool IsAPalindrome(string input) 
+          {
+            string compare = "";
+            foreach (var character in input.Reverse()) 
+            {
+                compare += character;
+            }
+            return (compare == input) ? true : false;
+          }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
         }
     }
 }
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,13 +1,23 @@
 using System;
 using Xunit;
+using System.Linq;
+using PalindromeExercise;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("madam", true)]
+        [InlineData("hello", false)]
+        public void IsWordPalindromeTest(string input, bool expected)
         {
+          WordSmith wordSmith = new WordSmith(); 
+
+          var actual = wordSmith.IsAPalindrome(input);  
+
+          Assert.Equal(expected, actual);
 
         }
     }
